ca1f03f27fc3a442b0440e9c0b50b309bf94c68b
Neeraj <neeraj.171it226@nitk.edu.in>
2019-11-29 12:52:19 +0530

    Added documentation

--- main.go
+++ main.go
@@ -13,19 +13,18 @@ import (
 
 	"github.com/philippgille/gokv/redis"
 )
-
+//struct for long url for accepting JSON
 type ORG_URL_Struct struct {
 	OrigURL string `json:"orig_url"`
 }
+//struct for short url for accepting JSON
 type Short_URL_Struct struct {
 	ShortURL string `json:"short_url"`
 }
 
-var m map[string]string
-var ini_value int
-
 func get_short_url(n uint64) string {
-	// fmt.Println(n)
+	//map n to a 62 bit number
+	//this will be the short URL
 	alphabet := string("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	s := ""
 	for n > 0 {
@@ -35,46 +34,45 @@ func get_short_url(n uint64) string {
 	return s
 }
 func createHandler(w http.ResponseWriter, r *http.Request) {
+	//handling the post request
 	org_url := ORG_URL_Struct{}
 	jsn, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal("Error reading the body", err)
 	}
+	//converting json to the struct
 	err = json.Unmarshal(jsn, &org_url)
 	if err != nil {
 		log.Fatal("Decoding error: ", err)
 	}
 	log.Printf("Received: %v\n", org_url)
-
 	//calculating md5 hash value of the long url to get the id value
+	//id_value will be passed to the get_short_url to get the 62 bit alphanumeric short url
 	bi := big.NewInt(0)
 	h := md5.New()
 	h.Write([]byte(org_url.OrigURL))
 	hexstr := hex.EncodeToString(h.Sum(nil))
 	bi.SetString(hexstr, 16)
-	// fmt.Println(bi.String())
 	id_value := bi.Uint64()
 	s := get_short_url(id_value)
 	short_url := Short_URL_Struct{
 		ShortURL: s,
 	}
+	//creating the json with short url
 	shortJson, err := json.Marshal(short_url)
-
 	if err != nil {
 		fmt.Fprintf(w, "Error: %s", err)
 	}
-	// fmt.Println("here is the original url")
-	// fmt.Println(org_url.OrigURL)
-
 	m[s] = org_url.OrigURL
 	//store key value in redis
 	options := redis.DefaultOptions // Address: "localhost:6379", Password: "", DB: 0
-	// Create client
+	// Create client to interact with redis server
 	client, err := redis.NewClient(options)
 	if err != nil {
 		panic(err)
 	}
 	defer client.Close()
+	//setting key s to value org_url.OrigURL, storing key value pair
 	err_ := client.Set(s, org_url.OrigURL)
 	if err_ != nil {
 		panic(err)
@@ -84,15 +82,14 @@ func createHandler(w http.ResponseWriter, r *http.Request) {
 	if err__ != nil {
 		panic(err)
 	}
-
-	// bb := *(retrievedVal)
-	// fmt.Println(bb)
 	w.Header().Set("Content-Type", "application/json")
+	//responding with json containing short-url
 	w.Write(shortJson)
 
 }
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Path
+	//extracting the short url
 	message = strings.TrimPrefix(message, "/url/")
 	fmt.Println("The short URL request is " + message)
 	_, ok := m[message]
@@ -104,13 +101,12 @@ func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer client.Close()
 	retrievedVal := new(string)
+	//get the value of the key message, basically a map from short url to long url
 	ok, err__ := client.Get(message, retrievedVal)
 	if err__ != nil {
 		panic(err)
 	}
-
 	bb := *(retrievedVal)
-
 	if ok == false {
 		text := "URL is invalid"
 		w.Write([]byte(text))
@@ -122,13 +118,13 @@ func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func server() {
+	//handler for creating short url from long url and storing
 	http.HandleFunc("/CREATE", createHandler)
+	//handler for looking up if the short url is valid and returning long url if the short url is valid
 	http.HandleFunc("/url/", redirectHandler)
 	http.ListenAndServe(":8047", nil)
 }
 
 func main() {
-	m = make(map[string]string)
-	ini_value = 10000
 	server()
 }
